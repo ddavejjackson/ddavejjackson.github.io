19cef44dd38135acf28e9068c2fff064ca55bbf0
ddavejjackson <protoblaster11@outlook.ie>
2026-03-02 14:20:54 +0000

    change mobile menu for animations

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,16 @@
-const btn = document.getElementById("menu-btn");
+const button = document.getElementById("menu-btn");
 const menu = document.getElementById("mobile-menu");
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
+button.addEventListener("click", () => {
+  menu.classList.toggle("scale-y-0");
+  menu.classList.toggle("scale-y-100");
+  menu.classList.toggle("opacity-0");
+  menu.classList.toggle("opacity-100");
+});
+
+document.querySelectorAll("#mobile-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.add("scale-y-0", "opacity-0");
+    menu.classList.remove("scale-y-100", "opacity-100");
+  });
 });
